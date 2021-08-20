0a8b94d7f44772318275f26125f53659b57cca40
AI20BTECH11014 <89123382+AI20BTECH11014@users.noreply.github.com>
2021-08-20 15:58:42 +0530

    Update Assignment_1.py

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -1,11 +1,26 @@
-#Import the libraries#
-import numpy as np 
+from numpy import array 
+from numpy.linalg import norm  
 import matplotlib.pyplot as plt
 from sympy import *
+
+def D(A,B):
+  Z = A-B
+  sum=0
+  for i in range(0,2):
+    sum=sum + pow(Z[i],2)
+  return sum
+P = np.array([-1,1]).reshape((2,1))
+Q = np.array([2,-1]).reshape((2,1))
+l_1= pow(D(P,Q),0.5)
+l_2= np.linalg.norm(P-Q)
+print('length using inbuilt function:',l_2 )
+print('length using D function:',l_1)
+print('We can observe that both the results are close to each other.')
 
 #To label the points  
 X = [-1,2]
 Y = [1,-1]
+
 #To get the line 
 X1 =[-1,2]
 Y1 = [1,-1]
